24e90ab8d022bb7a5eaa408a0073a0f101e791a6
Yuki Takasu <t.yuki0370@gmail.com>
2021-12-04 11:46:28 +0900

    heroku デプロイのセットアップ

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,4 @@
-# import django_heroku
+import django_heroku
 from pathlib import Path
 import os
 import environ
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls' #プロジェクト名をxxxに入れる
@@ -132,12 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
@@ -153,4 +153,4 @@ MEDIA_ROOT = 'media/'
 
 
 #Heroku
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
