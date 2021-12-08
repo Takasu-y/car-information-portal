313cf5729e8b6c8f69b47462c2271bad1761a46a
Yuki Takasu <t.yuki0370@gmail.com>
2021-12-08 10:02:54 +0900

    modelList pageとresult pageの実装

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, ModelListView, fetchAPI
+from .views import IndexView, ModelListView, ResultView
+
+app_name='portal'
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
-    path("modelList/make/", ModelListView.as_view(), name='modelList'),
-    path("fetchAPI/", fetchAPI, name="fetchAPI")
+    path("modelList/make/<str:maker>", ModelListView.as_view(), name='modelList'),
+    path("result/<str:maker>/<str:model>/", ResultView.as_view(), name='result'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,9 @@
-from json.decoder import JSONDecodeError
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 import requests
 import pprint
 import json
+
 
 from . import maker_logo
 
@@ -12,43 +12,29 @@ from . import maker_logo
 # Create your views here.
 
 
-def fetchAPI(request):
+def fetchAPI(make, model, min_year, max_year):
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36'
-    # url = ' https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims&make=Ford&year=2005'
-    url = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes&year=2020'
 
     header = {
         'User-Agent': user_agent,
         "content-type": "application/json"
     }
 
-
-
-
+    url = f'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims&make={make}&model={model}&min_year={min_year}&max_year={max_year}'
+    print(url)
     response = requests.get(url, headers=header)
 
     # str -> dict
-    models = json.loads(response.text[2:-2])['Makes']
+    dict = json.loads(response.text[2:-2])["Trims"]
 
-    # 集合に変換
-    setMakers = set()
-    for model in models:
-        setMakers.add(model["make_display"])
-
-    # pprint.pprint(setMakers)
-
-
-    context = {
-        'models': setMakers
-    }
-    return render(request, 'index.html', context)
+    return dict
 
 
 class IndexView(TemplateView):
 
     template_name = "index.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         logos = maker_logo.makers
 
         context = { "logos": logos }
@@ -63,4 +49,43 @@ class IndexView(TemplateView):
 class ModelListView(TemplateView):
 
     template_name = 'portal/modelList.html'
-    context_object_name = 'modelList'
+
+    def get(self, request, maker):
+
+        fetchModels = fetchAPI(maker, "", 2021, 2021)
+
+        setModels = set()
+        for model in fetchModels:
+            setModels.add(model["model_name"])
+
+        makerDict = {
+            'name': maker,
+            'logo': maker_logo.makers[maker]
+        }
+
+        context = {
+            "maker": makerDict,
+            "models":setModels
+        }
+        return render(request, self.template_name, context)
+
+
+
+class ResultView(TemplateView):
+    template_name = "portal/resultList.html"
+
+    def get(self, request, maker, model):
+
+        fetchData = fetchAPI(maker, model, 1886, 2021)
+
+        makerDict = {
+            'name': maker,
+            'logo': maker_logo.makers[maker]
+        }
+
+
+        context = {
+            "maker": makerDict,
+            "data": fetchData
+        }
+        return render(request, self.template_name, context)
